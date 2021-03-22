198380ede3e0c0f0fd48d3fe8871d2cd309b14c7
Sergei Lukaushkin <luks3r@gmail.com>
2021-03-22 10:33:11 +0200

    Updated monster ai system

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rltk::{GameState, Rltk, RGB};
+use rltk::{GameState, Rltk, RGB, Point};
 use specs::prelude::*;
 
 mod components;
@@ -16,16 +16,30 @@ pub use rect::*;
 mod visibility_system;
 use visibility_system::VisibilitySystem;
 
+mod monster_ai_system;
+use monster_ai_system::MonsterAI;
+
+#[derive(PartialEq, Clone, Copy)]
+pub enum RunState {
+    Paused,
+    Running,
+}
+
 pub struct State {
     ecs: World,
+    pub runstate: RunState,
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
 
-        player_input(self, ctx);
-        self.run_systems();
+        if self.runstate == RunState::Running {
+            self.run_systems();
+            self.runstate = RunState::Paused;
+        } else {
+            self.runstate = player_input(self, ctx);
+        }
 
         draw_map(&self.ecs, ctx);
 
@@ -46,9 +60,12 @@ impl State {
     fn run_systems(&mut self) {
         let mut vis = VisibilitySystem {};
         vis.run_now(&self.ecs);
+        let mut mob = MonsterAI {};
+        mob.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
+
 
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
@@ -56,13 +73,17 @@ fn main() -> rltk::BError {
         .with_title("Luksy Rouge")
         .build()?;
 
-    let mut gs = State { ecs: World::new() };
+    let mut gs = State {
+        ecs: World::new(),
+        runstate: RunState::Running,
+    };
 
     // Register components
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
     gs.ecs.register::<Viewshed>();
+    gs.ecs.register::<Monster>();
 
     // Create map
     let map = Map::new_map_rooms_and_corridors();
@@ -89,13 +110,23 @@ fn main() -> rltk::BError {
         .build();
 
     // Spawn monsters
+    let mut rng = rltk::RandomNumberGenerator::new();
     for room in map.rooms.iter().skip(1) {
         let (x, y) = room.center();
+
+        let glyph: rltk::FontCharType;
+        let roll = rng.roll_dice(1, 2);
+
+        match roll {
+            1 => glyph = rltk::to_cp437('g'),
+            _ => glyph = rltk::to_cp437('o'),
+        }
+
         gs.ecs
             .create_entity()
             .with(Position { x, y })
             .with(Renderable {
-                glyph: rltk::to_cp437('g'),
+                glyph,
                 fg: RGB::named(rltk::RED),
                 bg: RGB::named(rltk::BLACK),
             })
@@ -104,9 +135,11 @@ fn main() -> rltk::BError {
                 range: 8,
                 dirty: true,
             })
+            .with(Monster {})
             .build();
     }
 
     gs.ecs.insert(map);
+    gs.ecs.insert(Point::new(player_x, player_y));
     rltk::main_loop(context, gs)
 }
---
+++ src/monster_ai_system.rs
@@ -0,0 +1,23 @@
+use super::{Monster, Viewshed};
+use rltk::{console, Point};
+use specs::prelude::*;
+
+pub struct MonsterAI {}
+
+impl<'a> System<'a> for MonsterAI {
+    type SystemData = (
+        ReadExpect<'a, Point>,
+        ReadStorage<'a, Viewshed>,
+        ReadStorage<'a, Monster>,
+    );
+
+    fn run(&mut self, data: Self::SystemData) {
+        let (player_pos, viewshed, monster) = data;
+
+        for (viewshed, _monster) in (&viewshed, &monster).join() {
+            if viewshed.visible_tiles.contains(&*player_pos) {
+                console::log(format!("Monster shouts insults"))
+            }
+        }
+    }
+}
